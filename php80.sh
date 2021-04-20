0b31725baea9d6dc53298984b2dd4cdc34641187
Halil Şafak KILIÇ <halilsafakkilic@gmail.com>
2021-04-20 23:53:44 +0300

    PHP80.sh added.

---
+++ php80.sh
@@ -0,0 +1,20 @@
+# PHP (Ondrej)
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
+
+sudo apt-get install -y php8.0-fpm \
+php8.0-mbstring \
+php8.0-mysql \
+php8.0-xml \
+php8.0-zip \
+php8.0-intl \
+php8.0-gd \
+php8.0-bcmath \
+php8.0-curl \
+php8.0-xdebug \
+php8.0-soap \
+php8.0-mongodb \
+php8.0-yaml
+
+sudo apt-get install -y composer
